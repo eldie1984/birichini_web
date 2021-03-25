9957c663df2c0f35577a8b00adb3b82140af1daf
Diego Gasch <diego.gasch@mercadolibre.com>
2021-03-25 11:05:11 -0300

    update

--- instance/config.py
+++ instance/config.py
@@ -1,2 +1,2 @@
 SECRET_KEY = 'p9Bv<3Eid9%$i01'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///C:/Users/Daniel/birichini/birichini.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:////Users/dgasch/projects/birichini_web/birichini.db'
